f95d3daf85932b766fde702650b917aede3976b2
Destara Cipta <detajuventini@gmail.com>
2024-09-22 09:23:29 +0700

    first commit component

---
+++ components/navbar/NavSearch.tsx
@@ -0,0 +1,9 @@
+import { Input } from '../ui/input';
+
+function NavSearch() {
+	return (
+		<Input type='text' placeHolder='find a property...' className='max-w-xs dark:bg-muted' />
+		)
+}
+
+export default NavSearch;
