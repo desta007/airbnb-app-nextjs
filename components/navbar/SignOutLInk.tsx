2324c1ed32b3cda752d9c332933957946e79e387
Destara Cipta <detajuventini@gmail.com>
2024-09-28 21:47:14 +0700

    login and logout feature

--- components/navbar/SignOutLInk.tsx
+++ components/navbar/SignOutLInk.tsx
@@ -1,5 +1,19 @@
-function SignOutLink() {
-	return <div>SignOutLink</div>
-}
+'use client';
 
+import { SignOutButton } from '@clerk/nextjs';
+import { toast } from '@/hooks/use-toast';
+
+function SignOutLink() {
+  const handleLogout = () => {
+    toast({ description: 'You have been signed out.' });
+  };
+
+  return (
+    <SignOutButton redirectUrl='/'>
+      <button className='w-full text-left' onClick={handleLogout}>
+        Logout
+      </button>
+    </SignOutButton>
+  );
+}
 export default SignOutLink;
